1f5a7ebc9dea7f2af19d9cead726aaf20dad16b7
AryanshDev <aryanshappmaker@gmail.com>
2023-03-19 12:01:53 +0530

    Added Dummy Functionality, Generates Passwords Now

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,24 @@
-function generatePassword(len,use_Number,use_sym) {
+function generatePassword() {
+    var password = '';
+    let len = parseInt(document.getElementById("pass-len-input").value);
+    let arrayCharsCaps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    let arrayCharsSmall = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    for (let index = 0; index < len; index++) {
+        var what = Math.random();
+        if (what < 0.5){
+            var randomElement= arrayCharsCaps[Math.floor(Math.random() * 26)]; 
+            password += randomElement;
+        }
+        else{
+            var randomElement= arrayCharsSmall[Math.floor(Math.random() * 26)]; 
+            password += randomElement;
+        }
+    }
     var output = document.getElementById("pass-output-display");
-    output.value = "Hello World"
-    return "HELLO WORLD";
+    output.value = password;
+}
+
+function copyToClipboard() {
+    let password = document.getElementById("pass-output-display");
+    navigator.clipboard.writeText(password.value);
 }
